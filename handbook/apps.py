b02644f08cedbe45f7daba6c41cef8535ed60619
Gricana <alex2904.goryachev@yandex.com>
2024-11-28 17:48:26 +0300

    Initial commit

---
+++ handbook/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class HandbookConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'handbook'
+    verbose_name = _('Handbook')
