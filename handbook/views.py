1f06abbe793eceab497c0557d3ecbb26235dba7a
Gricana <alex2904.goryachev@yandex.com>
2024-12-02 16:32:42 +0300

    refactor: использован HandbookMixin в HandbookViewSet

--- handbook/views.py
+++ handbook/views.py
@@ -1,16 +1,17 @@
-from typing import List, Optional
+from typing import Optional
 
 from django.core.exceptions import ValidationError as DjangoValidationError
-from django.http import Http404
+from django.db.models import QuerySet
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import NotFound, ValidationError
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
 from .filters import HandbookElementFilter, HandbookFilter
-from .models import Handbook, HandbookElement, HandbookVersion
+from .mixins import HandbookMixin
+from .models import Handbook, HandbookElement
 from .schema import (
     check_element_schema,
     get_handbook_elements_schema,
@@ -19,7 +20,7 @@ from .schema import (
 from .serializers import HandbookElementSerializer, HandbookSerializer
 
 
-class HandbookViewSet(viewsets.ReadOnlyModelViewSet):
+class HandbookViewSet(HandbookMixin, viewsets.ReadOnlyModelViewSet):
     """
     ViewSet для работы со справочниками.
     Позволяет получать список справочников,
@@ -30,16 +31,18 @@ class HandbookViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filterset_class = HandbookFilter
 
-    def get_queryset(self) -> List[Handbook]:
+    def get_queryset(self) -> QuerySet[Handbook]:
         """
-        Возвращает базовый QuerySet справочников с оптимизацией связанных данных.
+        Возвращает QuerySet справочников с оптимизацией связанных данных.
+
+        :return: QuerySet справочников и связанные с ними версии.
         """
         return Handbook.objects.all().prefetch_related("versions").distinct()
 
     @swagger_auto_schema(**list_handbooks_schema)
     def list(self, request, *args, **kwargs) -> Response:
         """
-        Возвращает список справочников с поддержкой фильтрации и сортировки.
+        Возвращает список справочников с поддержкой фильтрации.
         """
         try:
             queryset = self.filter_queryset(self.get_queryset())
@@ -51,7 +54,8 @@ class HandbookViewSet(viewsets.ReadOnlyModelViewSet):
     @swagger_auto_schema(auto_schema=None)
     def retrieve(self, request, *args, **kwargs):
         """
-        Метод извлечения конкретного справочника удалён из схемы Swagger.
+        Метод извлечения конкретного справочника.
+        Удалён из схемы Swagger.
         """
         return super().retrieve(request, *args, **kwargs)
 
@@ -60,14 +64,11 @@ class HandbookViewSet(viewsets.ReadOnlyModelViewSet):
     def elements(self, request, pk=None) -> Response:
         """
         Возвращает элементы справочника по указанной или текущей версии.
+
+        :param pk: Идентификатор справочника.
+        :return: Ответ в JSON с элементами справочника.
         """
-        version_param = request.query_params.get("version")
-
-        handbook = self.get_handbook_or_404(pk)
-        version = self.get_version_or_404(handbook, version_param)
-
-        elements = version.elements.all()
-
+        elements = self.get_elements_by_version(request, pk)
         serializer = HandbookElementSerializer(elements, many=True)
         return Response({"elements": serializer.data})
 
@@ -76,13 +77,12 @@ class HandbookViewSet(viewsets.ReadOnlyModelViewSet):
     def check_element(self, request, pk=None) -> Response:
         """
         Проверяет наличие элемента с указанным кодом и значением в указанной версии.
-        """
-        handbook = self.get_handbook_or_404(pk)
-        version_param = request.query_params.get("version")
-        version = self.get_version_or_404(handbook, version_param)
 
-        queryset = HandbookElement.objects.filter(version=version)
-        filterset = HandbookElementFilter(request.GET, queryset=queryset)
+        :param pk: Идентификатор справочника.
+        :return: Ответ в JSON с ключом "exists", указывающим на наличие элемента.
+        """
+        elements = self.get_elements_by_version(request, pk)
+        filterset = HandbookElementFilter(request.GET, queryset=elements)
 
         if not filterset.is_valid():
             return Response(filterset.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -90,32 +90,16 @@ class HandbookViewSet(viewsets.ReadOnlyModelViewSet):
         exists = filterset.qs.exists()
         return Response({"exists": exists}, status=status.HTTP_200_OK)
 
-    def get_handbook_or_404(self, pk: int) -> Handbook:
+    def get_elements_by_version(
+        self, request, pk: Optional[int]
+    ) -> QuerySet[HandbookElement]:
         """
-        Получает справочник по его идентификатору или возвращает 404.
-        """
-        try:
-            return self.get_object()
-        except Http404:
-            raise NotFound({"error": "Handbook not found."})
+        Получает элементы справочника для указанной или текущей версии.
 
-    def get_version_or_404(
-        self, handbook: Handbook, version_param: Optional[str]
-    ) -> HandbookVersion:
+        :param pk: Идентификатор справочника.
+        :return: QuerySet элементов справочника.
         """
-        Возвращает указанную версию справочника или текущую версию.
-        """
-        if version_param:
-            try:
-                return handbook.versions.get(version=version_param)
-            except HandbookVersion.DoesNotExist:
-                raise NotFound(
-                    {"error": f"Version '{version_param}' not found for this handbook."}
-                )
-        else:
-            version = handbook.get_latest_version()
-            if not version:
-                raise NotFound(
-                    {"error": "No valid current version found for this handbook."}
-                )
-            return version
+        version_param = request.query_params.get("version")
+        handbook = self.get_handbook_or_404(pk)
+        version = self.get_version_or_404(handbook, version_param)
+        return version.elements.all()
